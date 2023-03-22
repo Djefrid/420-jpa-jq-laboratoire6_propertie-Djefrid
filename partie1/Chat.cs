b97fe791d0bfbe995bb5b8d55a7ec5cf78467bd1
Djefrid Byli Fotue Kuate <2210038@etu.cegepjonquiere.ca>
2023-03-21 21:08:30 -0400

    parti 1 commencer

---
+++ partie1/Chat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatNamespace
+{
+    internal class Chat
+    {
+        public  string Nom { get; set; }
+        public string Humeur { get; set; }
+
+        public Chat()
+        {
+            
+        }
+    }
+}
--- partie1/Program.cs
+++ partie1/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Lifetime;
 using System.Text;
 using System.Threading.Tasks;
+using ChatNamespace;
+using ChienNamespace;
 
-namespace partie1
+
+namespace AmitieNamespace
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Chat chat1 = new Chat
+            {
+                Nom = "Cat",
+                Humeur = "fachée"
+            };
+
+            Chat chat = new Chat
+            {
+                Nom = "Cat",
+                Humeur = "joyeux"
+            };
+
+            Chien chien1 = new Chien
+            {
+                Nom = "Dog",
+                Humeur = "joyeux"
+            };
+
+            Chien chien2 = new Chien
+            {
+                Nom = "Doggy",
+                Humeur = "joyeux"
+            };
+
+            if (chat1.Humeur == chien1.Humeur)
+                Console.WriteLine($"{chat.Nom} et {chien1.Nom} sont amie");
+            else
+                Console.WriteLine($"{chat.Nom} et {chien1.Nom} ne sont pas des amie");
+
+            if (chat.Humeur == chien1.Humeur)
+                Console.WriteLine($"{chat.Nom} et {chien1.Nom} sont amie");
+            else
+                Console.WriteLine($"{chat.Nom} et {chien1.Nom} ne sont pas des amie");
 
         }
     }
